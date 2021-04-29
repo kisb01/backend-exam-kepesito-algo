bcc7c79897b4e04e1108dfc013aaa850945af78e
kisb01 <kisb01@gmail.com>
2021-04-29 10:22:09 +0200

    Complete Algorithm project

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,34 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public static int addDigits(String input) {
+        if (input == null || input.length() == 0) {
+            return -1;
+        }
+        String[] justNumbers = input.replaceAll("[^0-9]", "").split("");
+        if (justNumbers[0].equals("")) {
+            return 0;
+        } else {
+            int[] inNumbers = inputsNumbersToArray(justNumbers);
+            int result = calculateSumOfDigits(inNumbers);
+            return result;
+        }
+    }
+
+    private static int calculateSumOfDigits(int[] inNumbers) {
+        int result = 0;
+        for (int i = 0; i < inNumbers.length; i++) {
+            result += inNumbers[i];
+        }
+        return result;
+    }
+
+    private static int[] inputsNumbersToArray(String[] justNumbers) {
+        int[] inNumbers = new int[justNumbers.length];
+        for (int i = 0; i < justNumbers.length; i++) {
+            inNumbers[i] = Integer.parseInt(justNumbers[i]);
+        }
+        return inNumbers;
+    }
 }
